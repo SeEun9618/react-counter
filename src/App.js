4901fb86bc486320065bc59995ceb51e40a136b7
seeun <youjeong960118@gmail.com>
2022-10-11 21:14:18 +0900

    Counter, Counter2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter.js';
+import Counter2 from './components/Counter2.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">
+      {/*<Counter />*/}
+      <Counter2 />
+  </div>;
 }
 
 export default App;
---
+++ src/components/Counter2.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+
+export default function Counter() {
+    const operators = ["+", "-", "*"];
+    const [operator, setOperator] = useState(operators[0]);
+
+    const [info, setInfo] = useState({
+        count : 0,
+        show : true,
+        operator : operators[0],
+    });
+
+    return (
+        <div>
+            <button onClick={() => {
+                let result;
+                if(info.operator === "+") result = info.count + 1;
+                if(info.operator === "-") result = info.count - 1;
+                if(info.operator === "*") result = info.count * 1;
+                setInfo({...info, count : result});
+                // setCount(result)
+            }}>{info.operator}1</button>
+            <button onClick={() => {
+                setInfo({...info, show : !info.show});
+            }}>Show and Hide!!</button>
+            <button onClick={() => {
+                const idx = Math.floor(Math.random() * operators.length);
+                setInfo({...info, operator: operators[idx]});
+            }}>Change Operator!!</button>
+            <br />
+            {info.show && `Counter : ${info.count}`}
+
+        </div>
+    );
+}
